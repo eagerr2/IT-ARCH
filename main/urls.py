3f3884c96bcc0281a58943f1c47e5430a337abec
Eamann Miled <130555562+Eamanmiled@users.noreply.github.com>
2024-12-02 21:42:32 +0000

    prototype basicaally done

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('shop/<str:categorystr>/', views.shop_view, name='shopbycategory'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('checkout/', views.checkout_view, name='checkout'),
-    path('shop/<str:categorystr>', views.shop_view, name='shopbycategory'),
+    path('clear_cart/', views.clear_cart, name='clear_cart'),
 ]
-
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 
 def home_view(request):
@@ -11,15 +11,44 @@ def login_view(request):
 def signup_view(request):
     return render(request, 'signup.html')
 
-def checkout_view(request):
-    return render(request, 'checkout.html')
-
 def shop_view(request, categorystr):
     productsbycategory = Product.objects.filter(category=categorystr)
 
-
     ctx = {
-        'detail' : categorystr,
-        'productsbycategory' : productsbycategory,
+        'detail': categorystr,
+        'productsbycategory': productsbycategory,
     }
     return render(request, 'shopbycategory.html', context=ctx)
+
+def add_to_cart(request, product_id):
+    
+    product = Product.objects.get(id=product_id)
+
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        cart[str(product_id)]['quantity'] += 1
+    else:
+        cart[str(product_id)] = {
+            'name': product.name,
+            'price': float(product.price),
+            'quantity': 1,
+        }
+
+    request.session['cart'] = cart
+
+    return redirect('home')
+
+def checkout_view(request):
+    cart = request.session.get('cart', {})
+    total = 0
+
+    for product_id, item in cart.items():
+        item['subtotal'] = item['price'] * item['quantity']
+        total += item['subtotal']
+
+    return render(request, 'checkout.html', {'cart': cart, 'total': total})
+
+def clear_cart(request):
+    request.session['cart'] = {}
+    return redirect('home')
